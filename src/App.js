5f84e1caccd3289019c2e2265edb394e575f7255
furaha707 <zxcvyein950823@gmail.com>
2024-01-06 18:02:39 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { motion } from 'framer-motion';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box 
+    initial={{ scale: 0 }}
+    animate={{ rotate: 180, scale: 1 }}
+    transition={{
+      type: "spring",
+      stiffness: 260,
+      damping: 20
+    }}
+    />
   );
 }
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+  border-radius:15px;
+`
 
 export default App;
